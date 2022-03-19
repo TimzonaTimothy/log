c7d6bcece601c8ba3f8740dcfe647ae4c18ed2f8
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2022-03-19 14:43:04 +0100

    Update settings.py

--- CMS/settings.py
+++ CMS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$5a%%cj78_@&x045mb1bqe1el%$&5bnxd%qi=w*=xqb*txl2u-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tranmix.herokuapp.com', '127.0.0.1']
 
